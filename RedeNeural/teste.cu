dc9559618574a393d9104d2258d19309f6381f20
EdilbertoC <edilbertoneto980@gmail.com>
2026-02-13 16:48:51 -0300

    feat (Test): Benchmark Parallelism

--- RedeNeural/teste.cu
+++ RedeNeural/teste.cu
@@ -1,23 +1,51 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
+#include <chrono>
 
-__global__ void test_cuda() {
-    printf("Thread: %d\n", threadIdx.x);
+__global__ void test_cuda(float* d_out, int iterations) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    float val = 0.0f;
+    for (int i = 0; i < iterations; i++) {
+        val += sinf(idx * i) * cosf(idx * i);
+    }
+
+    if (idx < 30720) {
+        d_out[idx] = val;
+    }
 }
 
 int main() {
+    int blocks_count = 120;
+    int threads_count = 256;
+    int times = 1;
+    int iterations = 10'000;
+
     cudaDeviceProp prop;
     cudaGetDeviceProperties(&prop, 0);
     printf("Device detected: %s\n", prop.name);
     printf("------------------------------------------------\n");
 
-    int blocks_count = 120;
-    int threads_count = 256;
-    test_cuda<<<blocks_count, threads_count>>>();
+    float *d_ptr;
+    float size = blocks_count * threads_count * sizeof(float);
+    cudaMalloc((void**)&d_ptr, size);
 
-    cudaDeviceSynchronize();
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
 
+    cudaEventRecord(start);
+    for (int i = 0; i < times; i++) {
+        test_cuda << <blocks_count, threads_count >> > (d_ptr, iterations);
+    }
+    cudaEventRecord(stop);
+    cudaFree(d_ptr);
+
+    cudaEventSynchronize(stop);
+    float benchmark = 0;
+    cudaEventElapsedTime(&benchmark, start, stop);
+
+    
     printf("------------------------------------------------\n");
-    printf("Test completed successfully.!\n");
+    printf("Test completed: %fms\n", benchmark);
     return 0;
 }
